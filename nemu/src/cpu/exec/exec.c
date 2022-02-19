cb9866e4be187e8b451f340a393b5dcdb1a637ae
tracer-ics2017 <tracer@njuics.org>
2022-02-19 17:26:45 +0800

    >  compile 191360 aqni Linux DESKTOP-QO6TNC3 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  17:26:45  up   0:40,  0 users,  load average: 0.00, 0.00, 0.00 c334a60751519947f483c9c8851fa56b61e9f798

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -235,7 +235,10 @@ void exec_wrapper(bool print_flag) {
 #ifdef DEBUG
   int instr_len = decoding.seq_eip - cpu.eip;
   sprintf(decoding.p, "%*.s", 50 - (12 + 3 * instr_len), "");
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wrestrict"
   strcat(decoding.asm_buf, decoding.assembly);
+#pragma GCC diagnostic pop
   Log_write("%s\n", decoding.asm_buf);
   if (print_flag) {
     puts(decoding.asm_buf);
