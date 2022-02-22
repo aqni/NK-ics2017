41bd5a85ced9ab3567f545f9ad3ae17d2d6eeaaf
tracer-ics2017 <tracer@njuics.org>
2022-02-22 20:29:41 +0800

    >  compile 191360 aqni Linux DESKTOP-QO6TNC3 5.10.60.1-microsoft-standard-WSL2 #1 SMP Wed Aug 25 23:20:18 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  20:29:41  up 1 day  6:47,  0 users,  load average: 0.10, 0.03, 0.01 583f2008b8ec355bd6c156ea6ae41627d7670e7d

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -270,6 +270,7 @@ int eval(int lidx, int ridx, bool *success) {
       if(inbracket) continue;
       
       int curr_prio=priority_of(tokens[i].type);
+      printf("cmp %d %d\n",curr_prio,priority);
       if(curr_prio>=priority){
         op=i;
         priority=curr_prio;
@@ -277,12 +278,15 @@ int eval(int lidx, int ridx, bool *success) {
     }
     assert(!inbracket);
     if(priority<0 ||(operands_num_of(tokens[op].type)<2 && op!=lidx)){
+      printf("Bad expression!\n"); 
       return *success=false;
     }
+    printf("op = %d\n",op);
     /* return the value */
 
 #define EVAL_VAL1 (eval(lidx, op - 1, success))
 #define EVAL_VAL2 (eval(op + 1, ridx, success))
+    printf("token.type = %d\n",tokens[op].type);
     switch (tokens[op].type) {
       default: return *success=false;
       case TK_MINUS: return - EVAL_VAL2;
