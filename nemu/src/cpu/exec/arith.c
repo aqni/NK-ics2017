a1f2409010ed1584ddbed18c97701e5626a656a9
tracer-ics2017 <tracer@njuics.org>
2022-03-28 21:23:53 +0800

    >  compile 191360 aqni Linux DESKTOP-QO6TNC3 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:23:53  up   0:28,  0 users,  load average: 0.05, 0.03, 0.00 38e1cb8f579287b8a537bd380ec9bb8a66fee35

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -8,7 +8,6 @@ make_EHelper(add) {
   rtl_sltu(&t0, &t2, &id_dest->val);
   rtl_set_CF(&t0);
   rtl_get_CF(&t0);
-  Log("t2:%#x,id_dest->val:%#x,t0:%#x",t2,id_dest->val,t0);
 
   operand_write(id_dest, &t2);
 
@@ -98,6 +97,7 @@ make_EHelper(adc) {
   rtl_sltu(&t0, &t2, &id_dest->val);
   rtl_or(&t0, &t3, &t0);
   rtl_set_CF(&t0);
+  Log("t2:%#x,id_dest->val:%#x,t0:%#x",t2,id_dest->val,t0);
 
   rtl_xor(&t0, &id_dest->val, &id_src->val);
   rtl_not(&t0);
