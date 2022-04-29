c060fd92ad5f8d7c376313521d3919128f9bc901
tracer-ics2017 <tracer@njuics.org>
2022-04-29 16:54:34 +0800

    >  compile 191360 aqni Linux aqni-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  16:54:34 up 3 days, 16:40,  3 users,  load average: 0.99, 0.26, 0.07 66f60d3e103d4514545fda2355198cbd93bc0eb2

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,7 +1,11 @@
 #include "common.h"
+#include "am.h"
 
 static _RegSet* do_event(_Event e, _RegSet* r) {
+  extern _RegSet* do_syscall(_RegSet *r);
   switch (e.event) {
+
+    case _EVENT_SYSCALL:return do_syscall(r);
     default: panic("Unhandled event ID = %d", e.event);
   }
 
--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,11 +1,16 @@
 #include "common.h"
 #include "syscall.h"
+#include "arch.h"
 
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
   a[0] = SYSCALL_ARG1(r);
+  a[1] = SYSCALL_ARG2(r);
+  a[2] = SYSCALL_ARG3(r);
+  a[3] = SYSCALL_ARG4(r);
 
   switch (a[0]) {
+    case SYS_none: SYSCALL_RET(r)=1;return r;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
