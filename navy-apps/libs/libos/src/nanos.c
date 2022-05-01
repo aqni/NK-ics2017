e6c1e6fea22fceb5990679b7fa7dcfae0d2b08cf
tracer-ics2017 <tracer@njuics.org>
2022-05-01 19:14:10 +0800

    >  run 191360 aqni Linux aqni-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  19:14:10 up 4 days, 18 min,  3 users,  load average: 0.00, 0.00, 0.00 a01d8c26e67090ed7cef49df8e4a860f9a6722b

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -40,7 +40,7 @@ void *_sbrk(intptr_t increment){
 }
 
 int _read(int fd, void *buf, size_t count) {
-  return _syscall_(SYS_read,(uintptr_t)buf,count,0);
+  return _syscall_(SYS_read,fd,(uintptr_t)buf,count);
 }
 
 int _close(int fd) {
