169fceb07e39773e903bf02872729fee8a1734c1
tracer-ics2017 <tracer@njuics.org>
2022-05-02 16:39:15 +0800

    >  run 191360 aqni Linux aqni-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  16:39:15 up 4 days,  6:44,  2 users,  load average: 1.36, 1.40, 0.91 62d461d61a53b13cd672f694a092ca95a06b372e

--- navy-apps/tests/bmp/main.cpp
+++ navy-apps/tests/bmp/main.cpp
@@ -3,21 +3,13 @@
 #include <ndl.h>
 #include <stdio.h>
 int main() {
-  printf("line pass:%d\n",__LINE__);
   NDL_Bitmap *bmp = (NDL_Bitmap*)malloc(sizeof(NDL_Bitmap));
-  printf("line pass:%d\n",__LINE__);
   NDL_LoadBitmap(bmp, "/share/pictures/projectn.bmp");
-  printf("line pass:%d\n",__LINE__);
   assert(bmp->pixels);
-  printf("line pass:%d\n",__LINE__);
   NDL_OpenDisplay(bmp->w, bmp->h);
-  printf("line pass:%d\n",__LINE__);
   NDL_DrawRect(bmp->pixels, 0, 0, bmp->w, bmp->h);
-  printf("line pass:%d\n",__LINE__);
   NDL_Render();
-  printf("line pass:%d\n",__LINE__);
   NDL_CloseDisplay();
-  printf("line pass:%d\n",__LINE__);
   while (1);
   return 0;
 }
