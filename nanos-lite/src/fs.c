2da1337645bf938c92ad12d84ad6203dbd948c39
tracer-ics2017 <tracer@njuics.org>
2022-05-02 17:16:50 +0800

    >  run 191360 aqni Linux aqni-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  17:16:50 up 4 days,  7:21,  2 users,  load average: 0.14, 0.31, 0.40 6cff28e0a4d96abab79a7f2e1cdb72b180f8cf84

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -1,34 +1,4 @@
-// #include "fs.h"
-
-// typedef struct {
-//   char *name;
-//   size_t size;
-//   off_t disk_offset;
-//   off_t open_offset;
-// } Finfo;
-
-// enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB, FD_EVENTS, FD_DISPINFO, FD_NORMAL};
-
-// /* This is the information about all files in disk. */
-// static Finfo file_table[] __attribute__((used)) = {
-//   {"stdin (note that this is not the actual stdin)", 0, 0},
-//   {"stdout (note that this is not the actual stdout)", 0, 0},
-//   {"stderr (note that this is not the actual stderr)", 0, 0},
-//   [FD_FB] = {"/dev/fb", 0, 0},
-//   [FD_EVENTS] = {"/dev/events", 0, 0},
-//   [FD_DISPINFO] = {"/proc/dispinfo", 128, 0},
-// #include "files.h"
-// };
-
-// #define NR_FILES (sizeof(file_table) / sizeof(file_table[0]))
-
 #include "fs.h"
-
-void ramdisk_read(void *buf, off_t offset, size_t len);
-void ramdisk_write(const void *buf, off_t offset, size_t len);
-void fb_write(const void *buf, off_t offset, size_t len);
-void dispinfo_read(void *buf, off_t offset, size_t len);
-size_t events_read(void *buf, size_t len);
 
 typedef struct {
   char *name;
@@ -53,8 +23,8 @@ static Finfo file_table[] __attribute__((used)) = {
 #define NR_FILES (sizeof(file_table) / sizeof(file_table[0]))
 void init_fs() {
   // TODO: initialize the size of /dev/fb
-  file_table[FD_FB].disk_offset=sizeof(uint32_t)*_screen.height*_screen.width;
-  file_table[FD_FB].open_offset=0;
+  file_table[FD_FB].size = _screen.width * _screen.height * sizeof(uint32_t);
+  file_table[FD_FB].open_offset = 0;
 }
 
 int fs_open(const char *pathname, int flags, int mode){
