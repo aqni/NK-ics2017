39c4eaf7c2f33f4b5c7b775a28754e5f006e42ad
tracer-ics2017 <tracer@njuics.org>
2022-05-02 16:45:03 +0800

    >  run 191360 aqni Linux aqni-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  16:45:03 up 4 days,  6:49,  2 users,  load average: 0.27, 0.71, 0.77 2d26a48c4d036db537f52f9c5ad01e810bc950d

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -1,7 +1,5 @@
 #include "common.h"
 
-extern int snprintf(char *s, size_t n, const char *format, ...);
-
 #define NAME(key) \
   [_KEY_##key] = #key,
 
@@ -11,41 +9,30 @@ static const char *keyname[256] __attribute__((used)) = {
 };
 
 size_t events_read(void *buf, size_t len) {
-  // int key_code;
-
-  // if ((key_code = _read_key()) == _KEY_NONE) {
-  //   snprintf(buf, len, "t %d\n", _uptime());
-  // } else if (key_code & 0x8000) {
-  //   key_code ^= 0x8000;
-  //   snprintf(buf, len, "kd %s\n", keyname[key_code]);
-  //   extern void switch_game();
-  //   if (key_code == _KEY_F12)
-  //     switch_game();
-  // } else {
-  //   snprintf(buf, len, "ku %s\n", keyname[key_code]);
-  // }
-  return strlen(buf);
+  return 0;
 }
 
 static char dispinfo[128] __attribute__((used));
+
+#define MYMIN(a,b) ((a)<(b)?(a):(b))
 
 void dispinfo_read(void *buf, off_t offset, size_t len) {
   memcpy(buf, dispinfo + offset, len);
 }
 
 void fb_write(const void *buf, off_t offset, size_t len) {
-  int row, col;
-  offset /= 4;
-  col = offset % _screen.width;
-  row = offset / _screen.width;
-
-  _draw_rect(buf, col, row, len/4, 1);
+  if(offset >= _screen.width * _screen.height){
+    assert(0);
+  }
+  
+  int x=offset%_screen.width,y=offset/ _screen.width;
+  _draw_rect(buf,x,y,len/sizeof(uint32_t),1);
 }
 
 void init_device() {
   _ioe_init();
 
-  // TODO: print the string to array `dispinfo` with the format
+  // DONE: print the string to array `dispinfo` with the format
   // described in the Navy-apps convention
-  sprintf(dispinfo, "WIDTH:%d\nHEIGHT:%d\n", _screen.width, _screen.height);
+  sprintf(dispinfo, "WIDTH:%d\nHEIGHT:%d\n",  _screen.width, _screen.height);
 }
