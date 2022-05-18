50fad9d717e16254eed1540d41981cbf2b1d4a49
tracer-ics2017 <tracer@njuics.org>
2022-05-18 17:00:24 +0800

    >  compile 191360 aqni Linux aqni-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  17:00:24 up 5 days, 20:16,  2 users,  load average: 0.00, 0.04, 0.03 ccac564f566e5a26969c16ffdd71b6bf8e232815

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -99,7 +99,15 @@ paddr_t page_translate(vaddr_t addr,bool write) {
 uint32_t vaddr_read(vaddr_t addr, int len) {
     if (cross_page(addr,len)) {
         /* this is a special case, you can handle it later. */
-        Assert(0,"vaddr_read cross_page");
+            union {
+        uint8_t bytes[4];
+        uint32_t dword;
+      } data = {0};
+      for (int i = 0; i < len; i++) {
+        paddr_t paddr = page_translate(addr + i, false);
+        data.bytes[i] = (uint8_t)paddr_read(paddr, 1);
+      }
+      return data.dword;
     }
     else {
         paddr_t paddr = page_translate(addr,false);
