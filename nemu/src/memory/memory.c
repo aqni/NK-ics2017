c4b68765e70b3139f2d7f5eb9c777ee86695b87f
tracer-ics2017 <tracer@njuics.org>
2022-05-18 16:43:35 +0800

    >  compile 191360 aqni Linux aqni-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  16:43:35 up 5 days, 19:59,  2 users,  load average: 0.00, 0.02, 0.00 ce5a65c1d34f6e2d91a52b57b3f76e71355d63e1

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -37,41 +37,62 @@ static inline bool cross_page(vaddr_t addr, int len){
 }
 
 paddr_t page_translate(vaddr_t addr,bool write) {
-  /* if 没有开启分页机制 */
-  if(!(cpu.cr0.paging)){
-    return addr;
+  // /* if 没有开启分页机制 */
+  // if(!(cpu.cr0.paging)){
+  //   return addr;
+  // }
+
+  // // Log("cpu.cr0:%x",cpu.cr0.val);
+
+  // // Log("access va:%x",addr);
+  
+  // /* 访问页目录表 */
+  // PDE *pdir=(PDE*)(((uint32_t)cpu.cr3.page_directory_base) << 12);
+  // PDE pde;
+  // intptr_t ppde=(intptr_t)&pdir[addr >> 22];
+  // // Log("pdx:%x",addr >> 22);
+  // pde.val=paddr_read(ppde,sizeof(PDE));
+  // // Log("pde:%x",pde.val);
+  // // Log("pde.present:%x",pde.present);
+  // assert(pde.present);
+  // pde.accessed=true;
+  
+  // /* 访问页表 */
+  // PTE *ptab=(PTE*)(pde.page_frame << 12);
+  // PTE pte;
+  // intptr_t ppte=(intptr_t)&ptab[(addr<<10) >> 22];
+  // // Log("ptx:%x",(addr<<10) >> 12);
+  // pte.val=paddr_read(ppte, sizeof(PTE));
+  // assert(pte.present);
+  // pte.accessed = true;
+  // if(write) pte.dirty=true;
+
+  // /* 写回页目录、页表 */
+  // paddr_write(ppde,sizeof(PDE),pde.val);
+  // paddr_write(ppte,sizeof(PTE),pte.val);
+
+  // intptr_t paddr= (pte.page_frame << 12) | (addr & PAGE_MASK);
+  // return paddr;
+
+  PDE pde, *pgdir;
+  PTE pte, *pgtab;
+  paddr_t paddr = addr;
+
+  if (cpu.cr0.protect_enable && cpu.cr0.paging) {
+    pgdir = (PDE *)(intptr_t)(cpu.cr3.page_directory_base << 12);
+    pde.val = paddr_read((intptr_t)&pgdir[(addr >> 22) & 0x3ff], 4);
+    assert(pde.present);
+    pde.accessed = 1;
+
+    pgtab = (PTE *)(intptr_t)(pde.page_frame << 12);
+    pte.val = paddr_read((intptr_t)&pgtab[(addr >> 12) & 0x3ff], 4);
+    assert(pte.present);
+    pte.accessed = 1;
+    pte.dirty = write ? 1 : pte.dirty;
+
+    paddr = (pte.page_frame << 12) | (addr & PAGE_MASK);
   }
 
-  // Log("cpu.cr0:%x",cpu.cr0.val);
-
-  // Log("access va:%x",addr);
-  
-  /* 访问页目录表 */
-  PDE *pdir=(PDE*)(((uint32_t)cpu.cr3.page_directory_base) << 12);
-  PDE pde;
-  intptr_t ppde=(intptr_t)&pdir[addr >> 22];
-  // Log("pdx:%x",addr >> 22);
-  pde.val=paddr_read(ppde,sizeof(PDE));
-  // Log("pde:%x",pde.val);
-  // Log("pde.present:%x",pde.present);
-  assert(pde.present);
-  pde.accessed=true;
-  
-  /* 访问页表 */
-  PTE *ptab=(PTE*)(pde.page_frame << 12);
-  PTE pte;
-  intptr_t ppte=(intptr_t)&ptab[(addr<<10) >> 22];
-  // Log("ptx:%x",(addr<<10) >> 12);
-  pte.val=paddr_read(ppte, sizeof(PTE));
-  assert(pte.present);
-  pte.accessed = true;
-  if(write) pte.dirty=true;
-
-  /* 写回页目录、页表 */
-  paddr_write(ppde,sizeof(PDE),pde.val);
-  paddr_write(ppte,sizeof(PTE),pte.val);
-
-  intptr_t paddr= (pte.page_frame << 12) | (addr & PAGE_MASK);
   return paddr;
 }
 
