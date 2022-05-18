c6aae445c4dece77ff602d66f38f42ffe2628e2b
tracer-ics2017 <tracer@njuics.org>
2022-05-18 16:21:36 +0800

    >  run 191360 aqni Linux aqni-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  16:21:36 up 5 days, 19:37,  2 users,  load average: 0.15, 0.08, 0.02 9f88ae1919116b9aa22af6bdf861bf5c8803f54a

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -5,19 +5,34 @@
 #define DEFAULT_ENTRY ((void *)0x8048000)
 
 uintptr_t loader(_Protect *as, const char *filename) {
-  int fd = fs_open(filename,0,0);
-  size_t len=fs_filesz(fd);
-  uint8_t *va = DEFAULT_ENTRY;
+  // int fd = fs_open(filename,0,0);
+  // size_t len=fs_filesz(fd);
+  // uint8_t *va = DEFAULT_ENTRY;
 
-  while(len>0){
-    void *pa =new_page();
-    _map(as,va,pa);
-    int read_len= len > PGSIZE ? PGSIZE:len;
-    read_len=fs_read(fd,va,read_len);
-    va += read_len;
-    len -= read_len;
+  // while(len>0){
+  //   void *pa =new_page();
+  //   _map(as,va,pa);
+  //   int read_len= len > PGSIZE ? PGSIZE:len;
+  //   read_len=fs_read(fd,va,read_len);
+  //   va += read_len;
+  //   len -= read_len;
+  // }
+
+  // fs_close(fd);
+  // return (uintptr_t)DEFAULT_ENTRY;
+    int fd = fs_open(filename, 0, 0);
+  size_t nbyte = fs_filesz(fd);
+  void *pa;
+  void *va;
+
+  Log("loaded: [%d]%s size:%d", fd, filename, nbyte);
+
+  void *end = DEFAULT_ENTRY + nbyte;
+  for (va = DEFAULT_ENTRY; va < end; va += PGSIZE) {
+    pa = new_page();
+    _map(as, va, pa);
+    fs_read(fd, pa, (end - va) < PGSIZE ? (end - va) : PGSIZE);
   }
 
-  fs_close(fd);
   return (uintptr_t)DEFAULT_ENTRY;
 }
