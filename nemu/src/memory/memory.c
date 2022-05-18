30578016dad6b979bf9273e47846d9ac04193119
tracer-ics2017 <tracer@njuics.org>
2022-05-18 16:13:17 +0800

    >  compile 191360 aqni Linux aqni-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  16:13:17 up 5 days, 19:29,  2 users,  load average: 0.01, 0.03, 0.00 8f4a7d8cc24358efdd457ea1392362589042ae9

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -60,7 +60,7 @@ paddr_t page_translate(vaddr_t addr,bool write) {
   /* 访问页表 */
   PTE *ptab=(PTE*)(pde.page_frame << 12);
   PTE pte;
-  intptr_t ppte=(intptr_t)&ptab[(addr<<10) >> 12];
+  intptr_t ppte=(intptr_t)&ptab[(addr<<10) >> 22];
   Log("ptx:%x",(addr<<10) >> 12);
   pte.val=paddr_read(ppte, sizeof(PTE));
   assert(pte.present);
